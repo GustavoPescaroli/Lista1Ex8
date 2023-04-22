7f4153e3a11d8aa8810ba615306cd08dd4335ae2
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-04-21 21:55:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double formulaI;
+            double formulaII;
+            double F;
+
+            Console.WriteLine("valor de F");
+            F = double.Parse(Console.ReadLine());
+
+            formulaI = F - 32;
+            Console.WriteLine("valor da formulaI {0}", formulaI);
+            formulaII = formulaI * 5 / 9;
+            Console.WriteLine("valor da convesão de celsius para Fahrenheit {0}", formulaII);
+        }
+    }
+}
